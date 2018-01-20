dcf62f2d249a6bda4ea0cae0a54aa5a15ef57186
Ashley Edds <ashley@edds.net>
2018-01-20 16:40:48 -0500

    basic html, global var

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    var player1 = null;
+    var player2 = null;
+
+    var player1Name = "";
+    var player2Name = "";
+
+    var yourPlayerName = "";
+
+    var player1Choice = "";
+    var player2Choice = "";
+
+    var turn = 1;
+})
